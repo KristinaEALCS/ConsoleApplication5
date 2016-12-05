5a0e79e14c6a39f539d1bf9077b3bff5774ac580
Elfka <Elfka@LAPTOP-7I8S5DSC>
2016-12-05 11:34:26 +0100

    Add project files.

---
+++ ConsoleApplication5/ProductTypeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class ProductTypeRepository
+    {
+        private Dictionary<int, ProductType> Repository = new Dictionary<int, ProductType>;
+
+        public bool AdjustPrice(ProductType productType, double price)
+        {
+
+        }
+       
+    }
+}
